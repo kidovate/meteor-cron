e13f453a9f3ea2e5a74be984d4e1f4a333964cc7
Christian Stewart <kidovate@gmail.com>
2013-09-04 19:08:45 -0700

    Add requirement for underscore

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  api.use('underscore', ['client', 'server']);
   api.add_files('cron.js', ['client', 'server']);
   if(api.export)
   	api.export('Cron');
